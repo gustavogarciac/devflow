536145daa43581a38bcd8ae7217be251969de8c8
gustavo <gustavogarciaci1@gmail.com>
2023-10-19 22:56:48 -0300

    chore: fixing lint errors to deploy

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -10,7 +10,6 @@ import {
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
-import path from "path";
 
 export async function getUserById(params: GetUserByIdParams) {
   try {
@@ -51,9 +50,9 @@ export async function deleteUser(params: DeleteUserParams) {
 
     // Delete questions, answers, comments, etc.
 
-    const userQuestionIds = await Question.find({
-      author: user._id,
-    }).distinct("_id");
+    // const userQuestionIds = await Question.find({
+    //   author: user._id,
+    // }).distinct("_id");
 
     //  Delete user questions
 
