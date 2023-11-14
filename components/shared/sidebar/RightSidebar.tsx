21c490d2aeda834bbaab47636bf2853aea1aa529
gustavo <gustavogarciaci1@gmail.com>
2023-11-14 18:39:23 -0300

    feat: hot posts and popular tags added to sidebar

--- components/shared/sidebar/RightSidebar.tsx
+++ components/shared/sidebar/RightSidebar.tsx
@@ -2,47 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTag from "../RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getPopularTags } from "@/lib/actions/tag.actions";
 
-const hotQuestions = [
-  { _id: 1, title: "How do I use express as a custom server in NextJS?" },
-  { _id: 2, title: "Cascading Deletes in SQLAlchemy?" },
-  { _id: 3, title: "How to Perfectly center a div with TailwindCSS?" },
-  {
-    _id: 4,
-    title:
-      "Best practices for data fetching in a Next.js application with Server-Side Rendering(SSR)?",
-  },
-  { _id: 5, title: "Redux Toolkit not updating state as expected" },
-];
-const popularTags = [
-  {
-    _id: "1",
-    name: "javascript",
-    totalQuestions: 2,
-  },
-  {
-    _id: "2",
-    name: "react",
-    totalQuestions: 5,
-  },
-  {
-    _id: "3",
-    name: "nodejs",
-    totalQuestions: 3,
-  },
-  {
-    _id: "4",
-    name: "next",
-    totalQuestions: 7,
-  },
-  {
-    _id: "5",
-    name: "ai",
-    totalQuestions: 8,
-  },
-];
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getPopularTags();
 
-const RightSidebar = () => {
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-16 overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
@@ -51,7 +17,7 @@ const RightSidebar = () => {
           {hotQuestions.map((question) => (
             <Link
               key={question._id}
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               className="group flex cursor-pointer items-center justify-between gap-7"
             >
               <p className="body-medium text-dark500_light700 group-hover:opacity-70">
